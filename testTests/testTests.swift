ca460ba6dcb8819ec983f4d6dc3f967993c311a9
Frank Gk <frank.glaser@hotmail.com>
2025-05-21 09:56:10 -0400

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Frank Glaser on 5/21/25.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
